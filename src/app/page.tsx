77195e0656ca75732dc0b8a7f03c4ede31b1420e
shaif-ali-javad <shaifalijavad60@gmail.com>
2024-08-21 00:18:03 +0530

    ldsk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,16 @@ import Whychoosus from "@/components/WhyChoosUs"
 import { InfiniteMovingCardsDemo } from "@/components/TestimonalCards";
 import CardHoverEffectDemo from "@/components/UpcomingWebinars";
 import Instructors from "@/components/Experts";
+import EXPLORE from "@/components/Explore";
+import FEATURED from "@/components/Featured";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
     <HeroSection/>
     {/* <BackgroundGradientDemo/> */}
+    <EXPLORE/>
+    <FEATURED/>
     <CardHoverEffectDemo/>
     <InfiniteMovingCardsDemo/>
     <Instructors/>
---
+++ src/components/Explore.tsx
@@ -0,0 +1,46 @@
+'use client'
+import Image from "next/image";
+import React from "react";
+import courseData from "@/data/explore.json"
+
+function page() {
+  return (
+    <div className="min-h-screen bg-black py-20 pt-1">
+      <p className="text-white text-center text-3xl pb-10">EXPLORE OUR COLLECTIONS</p>
+        <div className="flex flex-wrap justify-center">
+            {courseData.courses.map((course) => (
+                <div className="inter-var m-4" key={course.id}>
+                <div className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+                  <div className="w-full mt-4">
+                    <Image
+                      src={course.image}
+                      height="1000"
+                      width="1000"
+                      className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                      alt={course.title}
+                    />
+                  <div
+                    className="text-xl font-bold text-neutral-600 darktext-white mt-9"
+                  >
+                    {course.title}
+                  </div>
+                  <div
+                    className="text-neutral-500 text-sm max-w-sm mt-2 darktext-neutral-300"
+                  >
+                    {course.description}
+                  </div>
+                  <div
+                      className="px-4 py-2 mt-3 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold text-center"
+                    >
+                      Explore
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>  
+    </div>
+  )
+}
+
+export default page
