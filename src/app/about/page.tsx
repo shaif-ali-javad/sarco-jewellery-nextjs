ebde8bd376eb04fc58090085608469332df680c4
shaif-ali-javad <shaifalijavad60@gmail.com>
2024-08-24 01:30:55 +0530

    dkj

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,18 +5,28 @@ function page() {
     <div className='bg-black py-12 pt-36 text-white p-5'>
       <div>
         <p className='font-bold text-4xl mb-3'>ABOUT US</p>
-        <p>Noha gold is a trusted name in the industry for the past many years and the leading precious metals trading company in Dubai. The team has been diligently trained in the art of identifying, testing and analyzing precious metals. A highly reliable service provider of accurate evaluation and substantial. Sarco also offers quality gold bars, gold coins and Jewellery bars. Meeting the increasing demand of regional and international markets, Sarco supplies the finest precious metals to our clients with very friendly personal assistance. Sarco unveils transparent physical Jewellery trading activity and provides the most rewarding trading opportunities in precious metals in UAE. Easy, trustworthy and hassle free processing steps sets Noha gold a cut above the rest.</p>
-      </div>
-
-      <div className='p-6'>
-        <p className='font-bold text-4xl mb-3'>TRADE RESPONSIBILITY</p>
-        <p>With years of expertise in precious metals trading, Noha gold guarantees our customers a wide array of products and services.</p>
+        <p>Noha gold is a trusted name in the industry for the past many years and the leading precious metals trading company in Dubai. The team has been diligently trained in the art of identifying, testing and analyzing precious metals. A highly reliable service provider of accurate evaluation and substantial. Noha gold also offers quality gold bars, gold coins and Jewellery bars. Meeting the increasing demand of regional and international markets, Noha gold supplies the finest precious metals to our clients with very friendly personal assistance. Noha gold unveils transparent physical Jewellery trading activity and provides the most rewarding trading opportunities in precious metals in UAE. Easy, trustworthy and hassle free processing steps sets Noha gold a cut above the rest.</p>
       </div>
 
       <div className='p-6'>
         <p className='font-bold text-4xl mb-3'>VISION</p>
-        <p>Our company is having vision of expanding its presence very soon in the refinery segment with own brand named as Noha gold. In order to serve you better and reach a wider network, we have focused to launch our outlets all over the region.</p>
+        <p>The vision of the company is to become one of the best gold bullion supplier and trader in the region. We are always aware of the fact of developing and sustaining mutually trusting relationship between our institution and its clients, and we are confident enough from the benefits that flow from this trust. That’s why our abiding and enduring commitment to our clients has resulted in an impeccable achievement, and made us stand at the precedence of the international precious metals trading world.</p>
       </div>
+
+      <div className='p-6'>
+        <p className='font-bold text-4xl mb-3'>BUILT ON LEGACY</p>
+        <p>Building on this legacy and its extensive expertise in gold trading, Noha gold assures to provide you with various safe and secure solutions to invest physically in high-end bars of both gold and silver bullion, coming from our refinery, and from the world’s leading bullion brands. 
+        <br /><br />
+        If you are thinking about making physical investments in high-end bars of gold and silver bullion, Noha gold makes sure customers are in safe hands with no second guesses about the investments. The company provides safe and reliable options in physical trading. The company offers research-based analytical feedback for client satisfaction and provides the highest quality gold and silver bullion at the most competitive market price.
+        <br /><br />
+        Noha gold offers various products to their clients such as 100 gm gold bars, 10 Tola Suisse gold bars, 250 gm minted bars, 1 ounce Suisse gold pendants, 5 gm Suisse gold bars and Al Etihad gold bar 1 kg with utmost purity.</p>
+      </div>
+
+      <div className='p-6'>
+        <p className='font-bold text-4xl mb-3'>PLEDGE</p>
+        <p>Based on the values of reliability, integrity, and credibility, the company never failed nor will ever fail to disappoint its clients and believes in mutual trust and clarity when it comes to providing services. We are determined to make the company in becoming one of the best gold bullion suppliers and traders in Dubai, all while adhering to a value system that ensures top-notch customer satisfaction and a clear vision.</p>
+      </div>
+
     </div>
   )
 }
--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -5,11 +5,11 @@ import courseData from "@/data/feature.json"
 
 function page() {
   return (
-    <div className="min-h-screen bg-black py-20 pt-1">
-        <div className="flex flex-wrap justify-center">
+    <div className="min-h-screen bg-black py-20 px-5 max-w-screen">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-items-center">
             {courseData.courses.map((course) => (
                 <div className="inter-var m-4" key={course.id}> 
-                <div className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+                <div className="relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border width ">
                   <div className="w-full mt-4">
                     <Image
                       src={course.image}
@@ -19,7 +19,7 @@ function page() {
                       alt={course.title}
                     />
                   <div
-                    className="text-xl font-semi text-center text-neutral-600 darktext-white mt-9"
+                    className="text-xl text-center text-neutral-600 darktext-white mt-9 width"
                   >
                     {course.title}
                   </div>
