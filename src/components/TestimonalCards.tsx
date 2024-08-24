4b59f402b8a88349865f9e6e5463de11904ce37f
shaif-ali-javad <shaifalijavad60@gmail.com>
2024-08-25 05:11:31 +0530

    jhb

--- src/components/TestimonalCards.tsx
+++ src/components/TestimonalCards.tsx
@@ -5,7 +5,7 @@ import { InfiniteMovingCards } from "./ui/infinite-moving-cards";
 
 export function InfiniteMovingCardsDemo() {
   return (
-    <div className="h-[40rem] w-full dark:bg-black relative flex flex-col items-center justify-center overflow-hidden">
+    <div className="h-[18rem] w-full dark:bg-black relative flex flex-col items-center justify-center overflow-hidden">
     {/* <h2 className="text-3xl font-bold text-center mb-8 z-10">Hear our Harmony: Voices of success</h2> */}
     <div className="flex justify-center w-full overflow-hidden px-4 sm:px-6 lg:px-8">
         <div className="w-full max-w-6xl">
--- src/components/heroSection.tsx
+++ src/components/heroSection.tsx
@@ -10,11 +10,11 @@ function HeroSection() {
         className="-top-40 left-0 md:left-60 md:-top-20"
       />
         <div className="p-4 ralative z-10 w-full text-center">
-            <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">Noha gold</h1>
+            <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral to-neutral-400">Noha gold</h1>
             <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg mx-auto">Explore our broad range of gold bullion products. <br className="mt-50"/><div className="mt-2"></div> manufactured locally as per international standards.</p>
             {/* <div className="mt-4">
                 <Link href={"/service"}>
-                <Button
+                <Button 
                     borderRadius="1.75rem"
                     className="bg-black dark:bg-black text-black text-white border-neutral-800 dark:border-slate-800"
                 >
