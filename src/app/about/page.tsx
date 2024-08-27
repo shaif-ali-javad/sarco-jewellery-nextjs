7101ad14bac877af2d9cf9944b98daef9bb47afb
shaif-ali-javad <shaifalijavad60@gmail.com>
2024-08-27 21:29:30 +0530

    dj

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -14,7 +14,7 @@ function page() {
     <div className='m-auto mt-10'>
       
       <div className="h-[10rem] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
-      <h1 className="md:text-7xl text-3xl lg:text-4xl font-bold text-center text-white relative z-20">
+      <h1 className="md:text-7xl text-3xl lg:text-4xl font-medium text-center text-white relative z-20">
       ABOUT US
       </h1>
       <div className="w-[40rem] h-10 relative">
@@ -34,13 +34,13 @@ function page() {
           particleColor="#FFFFFF"
         /> */}
 
-        <p className='font-bold text-4xl mb-3 text-neutral-400'></p>
+        <p className='font-medium text-4xl mb-3 text-neutral-400'></p>
         
         {/* Radial Gradient to prevent sharp edges */}
         {/* <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div> */}
       </div>
     </div>
-        {/* <p className='font-bold text-4xl mb-3 text-neutral-400'>PLEDGE</p> */}
+        {/* <p className='font-medium text-4xl mb-3 text-neutral-400'>PLEDGE</p> */}
         <p className='text-neutral-500 text-center descriptin'>Noha gold is a trusted name in the industry for the past many years and the leading precious metals trading company in Dubai. The team has been diligently trained in the art of identifying, testing and analyzing precious metals. A highly reliable service provider of accurate evaluation and substantial. Noha gold also offers quality gold bars, gold coins and Jewellery bars. Meeting the increasing demand of regional and international markets, Noha gold supplies the finest precious metals to our clients with very friendly personal assistance. Noha gold unveils transparent physical Jewellery trading activity and provides the most rewarding trading opportunities in precious metals in UAE. Easy, trustworthy and hassle free processing steps sets Noha gold a cut above the rest.</p>
       </div>
     
@@ -49,7 +49,7 @@ function page() {
       <div className='m-auto mt-10'>
       
       <div className="h-[10rem] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
-      <h1 className="md:text-7xl text-3xl lg:text-4xl font-bold text-center text-white relative z-20">
+      <h1 className="md:text-7xl text-3xl lg:text-4xl font-medium text-center text-white relative z-20">
       VISION
       </h1>
       <div className="w-[40rem] h-10 relative">
@@ -69,13 +69,13 @@ function page() {
           particleColor="#FFFFFF"
         /> */}
 
-        <p className='font-bold text-4xl mb-3 text-neutral-400'></p>
+        <p className='font-medium text-4xl mb-3 text-neutral-400'></p>
         
         {/* Radial Gradient to prevent sharp edges */}
         {/* <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div> */}
       </div>
     </div>
-        {/* <p className='font-bold text-4xl mb-3 text-neutral-400'>PLEDGE</p> */}
+        {/* <p className='font-medium text-4xl mb-3 text-neutral-400'>PLEDGE</p> */}
         <p className='text-neutral-500 text-center descriptin'>The vision of the company is to become one of the best gold bullion supplier and trader in the region. We are always aware of the fact of developing and sustaining mutually trusting relationship between our institution and its clients, and we are confident enough from the benefits that flow from this trust. That’s why x  our abiding and enduring commitment to our clients has resulted in an impeccable achievement, and made us stand at the precedence of the international precious metals trading world.</p>
       </div>
     
@@ -86,7 +86,7 @@ function page() {
       <img className='rounded-xl my-7 mt-16' src="https://img.freepik.com/premium-photo/brain-illustration_862994-508773.jpg?w=900" alt="" /> */}
       <div className='m-auto mt-10'>
       <div className="h-[10rem] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
-      <h1 className="md:text-7xl text-3xl lg:text-4xl font-bold text-center text-white relative z-20">
+      <h1 className="md:text-7xl text-3xl lg:text-4xl font-medium text-center text-white relative z-20">
       BUILT ON LEGACY
       </h1>
       <div className="w-[40rem] h-10 relative">
@@ -106,13 +106,13 @@ function page() {
           particleColor="#FFFFFF"
         /> */}
 
-        {/* <p className='font-bold text-4xl mb-3 text-neutral-400'></p> */}
+        {/* <p className='font-medium text-4xl mb-3 text-neutral-400'></p> */}
         
         {/* Radial Gradient to prevent sharp edges */}
         {/* <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div> */}
       </div>
     </div>
-        {/* <p className='font-bold text-4xl mb-3 text-neutral-400'>BUILT ON LEGACY</p> */}
+        {/* <p className='font-medium text-4xl mb-3 text-neutral-400'>BUILT ON LEGACY</p> */}
         <p className='text-neutral-500 text-center descriptin'>Building on this legacy and its extensive expertise in gold trading, Noha gold assures to provide you with various safe and secure solutions to invest physically in high-end bars of both gold and silver bullion, coming from our refinery, and from the world’s leading bullion brands. 
         <br /><br />
         If you are thinking about making physical investments in high-end bars of gold and silver bullion, Noha gold makes sure customers are in safe hands with no second guesses about the investments. The company provides safe and reliable options in physical trading. The company offers research-based analytical feedback for client satisfaction and provides the highest quality gold and silver bullion at the most competitive market price.
@@ -125,7 +125,7 @@ function page() {
       <img className='rounded-xl my-7 mt-16' src="https://img.freepik.com/free-photo/portrait-business-people-worker-team-outside_1150-2663.jpg?t=st=1724674079~exp=1724677679~hmac=53b63eb8c143edd4fb0aa603853d3e423ab8e19574c65bddef45fcb551ef7cc7&w=900" alt="" /> */}
       <div className='m-auto mt-10'>
       <div className="h-[10rem] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
-      <h1 className="md:text-7xl text-3xl lg:text-4xl font-bold text-center text-white relative z-20">
+      <h1 className="md:text-7xl text-3xl lg:text-4xl font-medium text-center text-white relative z-20">
       PLEDGE
       </h1>
       <div className="w-[40rem] h-10 relative">
@@ -145,13 +145,13 @@ function page() {
           particleColor="#FFFFFF"
         /> */}
 
-        <p className='font-bold text-4xl mb-3 text-neutral-400'></p>
+        <p className='font-medium text-4xl mb-3 text-neutral-400'></p>
         
         {/* Radial Gradient to prevent sharp edges */}
         {/* <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div> */}
       </div>
     </div>
-        {/* <p className='font-bold text-4xl mb-3 text-neutral-400'>PLEDGE</p> */}
+        {/* <p className='font-medium text-4xl mb-3 text-neutral-400'>PLEDGE</p> */}
         <p className='text-neutral-500 text-center descriptin'>Based on the values of reliability, integrity, and credibility, the company never failed nor will ever fail to disappoint its clients and believes in mutual trust and clarity when it comes to providing services. We are determined to make the company in becoming one of the best gold bullion suppliers and traders in Dubai, all while adhering to a value system that ensures top-notch customer satisfaction and a clear vision.</p>
       </div>
     </div>
