d25062b831d72d37fdf1582adbbe5f617b42a96e
shaif-ali-javad <shaifalijavad60@gmail.com>
2024-08-27 18:21:29 +0530

    in

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -38,7 +38,7 @@ function page() {
         <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
       </div>
     </div>
-        <p className='text-neutral-500 text-center'>Noha gold is a trusted name in the industry for the past many years and the leading precious metals trading company in Dubai. The team has been diligently trained in the art of identifying, testing and analyzing precious metals. A highly reliable service provider of accurate evaluation and substantial. Noha gold also offers quality gold bars, gold coins and Jewellery bars. Meeting the increasing demand of regional and international markets, Noha gold supplies the finest precious metals to our clients with very friendly personal assistance. Noha gold unveils transparent physical Jewellery trading activity and provides the most rewarding trading opportunities in precious metals in UAE. Easy, trustworthy and hassle free processing steps sets Noha gold a cut above the rest.</p>
+        <p className='text-neutral-500 text-center descriptin'>Noha gold is a trusted name in the industry for the past many years and the leading precious metals trading company in Dubai. The team has been diligently trained in the art of identifying, testing and analyzing precious metals. A highly reliable service provider of accurate evaluation and substantial. Noha gold also offers quality gold bars, gold coins and Jewellery bars. Meeting the increasing demand of regional and international markets, Noha gold supplies the finest precious metals to our clients with very friendly personal assistance. Noha gold unveils transparent physical Jewellery trading activity and provides the most rewarding trading opportunities in precious metals in UAE. Easy, trustworthy and hassle free processing steps sets Noha gold a cut above the rest.</p>
       </div>
       {/* </div> */}
 
@@ -73,7 +73,7 @@ function page() {
       </div>
     </div>
         {/* <p className='font-bold text-4xl mb-3 text-neutral-400'>VISION</p> */}
-        <p className='text-neutral-500 text-center'>The vision of the company is to become one of the best gold bullion supplier and trader in the region. We are always aware of the fact of developing and sustaining mutually trusting relationship between our institution and its clients, and we are confident enough from the benefits that flow from this trust. That’s why our abiding and enduring commitment to our clients has resulted in an impeccable achievement, and made us stand at the precedence of the international precious metals trading world.</p>
+        <p className='text-neutral-500 text-center descriptin'>The vision of the company is to become one of the best gold bullion supplier and trader in the region. We are always aware of the fact of developing and sustaining mutually trusting relationship between our institution and its clients, and we are confident enough from the benefits that flow from this trust. That’s why our abiding and enduring commitment to our clients has resulted in an impeccable achievement, and made us stand at the precedence of the international precious metals trading world.</p>
       </div>
     {/* </div> */}
 
@@ -109,7 +109,7 @@ function page() {
       </div>
     </div>
         {/* <p className='font-bold text-4xl mb-3 text-neutral-400'>BUILT ON LEGACY</p> */}
-        <p className='text-neutral-500 text-center'>Building on this legacy and its extensive expertise in gold trading, Noha gold assures to provide you with various safe and secure solutions to invest physically in high-end bars of both gold and silver bullion, coming from our refinery, and from the world’s leading bullion brands. 
+        <p className='text-neutral-500 text-center descriptin-0'>Building on this legacy and its extensive expertise in gold trading, Noha gold assures to provide you with various safe and secure solutions to invest physically in high-end bars of both gold and silver bullion, coming from our refinery, and from the world’s leading bullion brands. 
         <br /><br />
         If you are thinking about making physical investments in high-end bars of gold and silver bullion, Noha gold makes sure customers are in safe hands with no second guesses about the investments. The company provides safe and reliable options in physical trading. The company offers research-based analytical feedback for client satisfaction and provides the highest quality gold and silver bullion at the most competitive market price.
         <br /><br />
@@ -148,7 +148,7 @@ function page() {
       </div>
     </div>
         {/* <p className='font-bold text-4xl mb-3 text-neutral-400'>PLEDGE</p> */}
-        <p className='text-neutral-500 text-center'>Based on the values of reliability, integrity, and credibility, the company never failed nor will ever fail to disappoint its clients and believes in mutual trust and clarity when it comes to providing services. We are determined to make the company in becoming one of the best gold bullion suppliers and traders in Dubai, all while adhering to a value system that ensures top-notch customer satisfaction and a clear vision.</p>
+        <p className='text-neutral-500 text-center descriptin'>Based on the values of reliability, integrity, and credibility, the company never failed nor will ever fail to disappoint its clients and believes in mutual trust and clarity when it comes to providing services. We are determined to make the company in becoming one of the best gold bullion suppliers and traders in Dubai, all while adhering to a value system that ensures top-notch customer satisfaction and a clear vision.</p>
       </div>
     {/* </div> */}
 
