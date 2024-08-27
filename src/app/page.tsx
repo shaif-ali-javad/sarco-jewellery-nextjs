03ef7ff6e159f26843e4f6114d21ee4e18b29918
shaif-ali-javad <shaifalijavad60@gmail.com>
2024-08-27 21:41:33 +0530

    jyg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
-import BackgroundGradientDemo from "@/components/featured-services";
+// import BackgroundGradientDemo from "@/components/featured-services";
 import HeroSection from "@/components/heroSection";
-import Image from "next/image";
+// import Image from "next/image";
 // import Whychoosus from "@/components/WhyChoosUs"
 import { InfiniteMovingCardsDemo } from "@/components/TestimonalCards";
 import CardHoverEffectDemo from "@/components/UpcomingWebinars";
 import Instructors from "@/components/Experts";
 import EXPLORE from "@/components/Explore";
-import FEATURED from "@/components/Featured";
+// import FEATURED from "@/components/Featured";
 import FEATURE from "@/components/Feature";
 import Heritage from "@/components/Heritage";
-import Whygold from "@/components/Whygold";
+// import Whygold from "@/components/Whygold";
 
 export default function Home() {
   return (
